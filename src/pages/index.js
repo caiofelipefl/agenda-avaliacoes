bff510cbbe8b6bf80afea804fb60d6cad14e96ee
caiofelipefl <caiofelipefl28@gmail.com>
2022-10-06 21:08:51 -0300

    Reinstalação Tailwind e inserção do formulário

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+export default function Home() {
+  const [date, setDate] = useState(new Date())
+  
+  return (
+
+      <div classNameName='bg-red-200 flex-col px-4 py-4 flex items-center justify-center w-auto'>
+        <div>
+          <h1 className='text-3xl'>Calendário de Provas</h1>
+        </div>
+        <div className='mt-4 w-full border rounded'>
+          <div className='flex '>           
+              <form>
+                <div class="mb-6">
+                <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Disciplina</label>
+                  <input type="text" id="disciplina" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Programação Web" required=""/>
+                </div>
+                <div class="mb-6">
+                  <label for="date" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Data</label>
+                  <input type="date" id="data" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                </div>
+                <div class="mb-6">
+                <label for="time" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Horário</label>
+                  <input type="time" id="horario" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
+                </div>
+                <div class="mb-6">
+                  <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Descritivo</label>
+                  <input type="text" id="descritivo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Conteúdo - Hibernate e CSS" required=""/>
+                </div>
+      
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agendar</button>
+              </form>
+
+          </div>
+        </div>
+      </div>
+
+  )
+}
---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+export default function Login(){
+    return (
+        <h1 className="font-bold underline">Teste</h1>
+    )
+}
