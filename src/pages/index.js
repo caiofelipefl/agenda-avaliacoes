1938c438c90f9b433a6ab7f141bad43ea2645b45
caiofelipefl <caiofelipefl28@gmail.com>
2022-10-06 21:40:02 -0300

    Inserção e organização da segunda div

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,8 +10,8 @@ export default function Home() {
           <h1 className='text-3xl'>Calendário de Provas</h1>
         </div>
         <div className='mt-4 w-full border rounded'>
-          <div className='flex '>           
-              <form>
+            <div className='flex items-stretch justify-between w-full bg-blue-200'>           
+              <form className='bg-green-200 w-1/2'>
                 <div class="mb-6">
                 <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Disciplina</label>
                   <input type="text" id="disciplina" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Programação Web" required=""/>
@@ -26,14 +26,17 @@ export default function Home() {
                 </div>
                 <div class="mb-6">
                   <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Descritivo</label>
-                  <input type="text" id="descritivo" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Conteúdo - Hibernate e CSS" required=""/>
+                  <input type="text" id="descricao" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Conteúdo - Hibernate e CSS" required=""/>
                 </div>
-      
+       
                 <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agendar</button>
               </form>
-
+              <div className='bg-red-200 w-1/2'>
+                
+              </div>
+            </div>
+         
           </div>
-        </div>
       </div>
 
   )
