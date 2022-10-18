c85762b33b2a3a65bc0cb0b05011c14ff74ec3ec
caiofelipefl <caiofelipefl28@gmail.com>
2022-10-18 12:55:15 -0300

    Restauração pós erro com Prisma

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,29 +19,29 @@ export default function Home() {
   
   return (
 
-      <div className='bg-red-200 flex-col px-4 py-4 flex items-center justify-center w-auto'>
+      <div className='flex-col px-4 py-4 flex items-center justify-center w-auto'>
         <div>
-          <h1 className='text-3xl'>Calendário de Provas</h1>
+          <h1 className='text-3xl font-bold'>Calendário de Provas</h1>
         </div>
-        <div className='mt-4 w-full h-200 border rounded'>
-            <div className='flex items-stretch justify-between w-1/2 bg-blue-200'>           
-              <form onSubmit={HandleForm} className='bg-green-200 w-full justify-center'>
+        <div className='mt-4 w-full h-200 border border-emerald-500 rounded-2xl py-6 px-6'>
+            <div className='flex items-stretch justify-between w-1/2'>           
+              <form onSubmit={HandleForm} className='w-full justify-center'>
                 <div className="mb-6">
-                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Disciplina</label>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600">Disciplina</label>
                   <input onChange={(e)=>setDisciplina(e.target.value)} type="text" id="disciplina" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Programação Web" required=""/>
                 </div>
                 <div className="mb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Data e Hora</label>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600">Data e Hora</label>
                   <input onChange={(e)=>setData(e.target.value)} type="datetime-local" id="datahora" className="bg-gray-50 borderf border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
                 <div className="mb-6">
-                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Descritivo (Opcional)</label>
+                  <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-600">Descritivo (Opcional)</label>
                   <textarea onChange={(e)=>setDescritivo(e.target.value)}type="text" id="descritivo" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Ex: Conteúdo - Hibernate e CSS" required=""/>
                 </div>
        
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Agendar</button>
+                <button type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto ml-40 px-9 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Agendar</button>
               </form>
-              <div className='bg-red-200 w-1/2'>
+              <div className='w-1/2'>
                 
               </div>
             </div>
